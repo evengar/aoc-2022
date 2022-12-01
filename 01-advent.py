df0d5a4875e9cc2f7b1a52c763e75b0ff4addb47
Even Sletteng Garvang <evengar@uio.no>
2022-12-01 12:01:17 +0100

    removed counting of elves, because it wasn't necessary

--- 01-advent.py
+++ 01-advent.py
@@ -1,14 +1,10 @@
 # Part 1
 with open("data/01-input.txt", "r") as file:
     lines = file.readlines()
-    elf = 1
-    elves = []
     cal = 0
     calsums = []
     for line in lines:
         if len(line.strip()) == 0:
-            elf +=1
-            elves.append(elf)
             calsums.append(cal)
             cal = 0
             continue
