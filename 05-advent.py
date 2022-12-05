74b38d956eb40f2be72c2074c65015656f3b23fe
Even Sletteng Garvang <evengar@uio.no>
2022-12-05 09:57:31 +0100

    completed part 1

---
+++ 05-advent.py
@@ -0,0 +1,42 @@
+# Part 1
+
+crates = [[] for _ in range(9)]
+instructions = []
+
+with open("data/05-input.txt") as file:
+    for line in file:
+        if "[" in line:
+            for i in range(0, len(line), 4):
+                if not "   " in line[i:i+3]:
+                    crates[int(i/4)].append(line[i:i+3])
+        if "move" in line:
+            instructions.append(line.strip())
+
+#easier to work with if top crate is last in list
+crates_rev = [x[::-1] for x in crates]
+
+
+def parse_instructions(line):
+    n, orig, dest = [int(x) for x in line.split(" ")[1::2]]
+    return (n, orig, dest)
+
+def move_crates(crates, instructions):
+    n, orig, dest = parse_instructions(instructions)
+    for i in range(n):
+        crates[dest - 1].append(crates[orig - 1][-1])
+        del crates[orig - 1][-1]
+    return crates
+
+
+
+def CrateMover9000(crates, instructions):
+    for instruction in instructions:
+        crates = move_crates(crates, instruction)
+    return(crates)
+    
+final_crates = CrateMover9000(crates_rev, instructions)
+result = "".join([crate[-1][1] for crate in final_crates])
+print(result)
+
+# Part 2
+
